0ee9cd13115543438c61705466d9782b22fadbcf
claussimar <dasunheimliche7@gmail.com>
2025-09-23 21:55:43 -0300

    askdjf

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,10 @@
 export const getApiUrl = () => {
-  console.log("API URL:", import.meta.env.VITE_SKOOB_API);
+  const envUrl = import.meta.env.VITE_SKOOB_API;
+  const dynamicUrl = `${window.location.protocol}//${window.location.hostname}:3030`;
 
-  return (
-    import.meta.env.VITE_SKOOB_API ||
-    `${window.location.protocol}//${window.location.hostname}:3030`
-  );
+  console.log("ENV URL:", envUrl);
+  console.log("Dynamic URL:", dynamicUrl);
+  console.log("Final URL:", envUrl || dynamicUrl);
+
+  return envUrl || dynamicUrl;
 };
