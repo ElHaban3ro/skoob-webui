b1e0aca26dace6fa831ebdae7a3d95033a98e677
Fernando Leguizamo <ferdhaban@gmail.com>
2025-09-27 22:56:47 -0500

    Reestructuración del nav de my library

--- src/assets/components/minicomponents/library.tsx
+++ src/assets/components/minicomponents/library.tsx
@@ -1,13 +1,48 @@
 import type { User } from "@/types/user";
 import Header from "./header";
 import MyLibrary from "./my_library";
+import { useState } from "react";
+import { getApiUrl } from "@/config/api";
+import { useEffect } from "react";
+import LibraryNav from "./library_nav";
 
 function Library({ user }: { user: User | undefined }) {
-  return (
-    <div className="appmaindiv h-screen w-full flex flex-col justify-start items-center">
-      <Header user={user} />
-      <MyLibrary />
-    </div>
-  );
+    const [books, setBooks] = useState([]);
+    const api_url = getApiUrl();
+
+    const get_books = async () => {
+        await fetch(api_url + "/books/all", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+        })
+            .catch(() => {
+                setBooks([])
+            })
+            .then(async (res) => {
+                if (res) {
+                    if (res.status === 200) {
+                        const data = await res.json();
+                        setBooks(data.content.content);
+                    } else {
+                        setBooks([])
+                    }
+                }
+            });
+    };
+
+    useEffect(() => {
+        get_books();
+    }, [])
+
+    return (
+        <div className="appmaindiv h-screen w-full flex flex-col justify-start items-center">
+            <Header user={user} />
+            <LibraryNav get_books={get_books} />
+            <MyLibrary books={books} />
+        </div>
+    );
 }
 export default Library;
